523d3effbb896575807582d6fb5525162b3b9b37
MortezaGhoddousi <143504966+MortezaGhoddousi@users.noreply.github.com>
2024-01-20 17:33:00 +0330

    Added PowerPoints and session 01(Python)

---
+++ Session 01/session01.py
@@ -0,0 +1,36 @@
+print('Hello world!')
+
+## Variables
+# String
+Username = 'Morteza'
+
+print(Username)
+print(type(Username))
+
+# Integer
+
+age = 29
+print(age)
+print(type(age))
+
+# Float
+
+score = 18.94
+print(score)
+print(type(score))
+
+# Boolean
+
+isMale = True
+print(isMale)
+print(type(isMale))
+
+## Working with Variables
+
+price = 10
+price = 9
+discount = 0.1
+print(price*(1-discount))
+
+
+
